c129fe2383c0644cc99105a5d2b204b586d029a8
= <=>
2023-07-24 20:28:49 +0000

    Initial commit

---
+++ musix/src/main/java/is/musix/MusiXApplication.java
@@ -0,0 +1,13 @@
+package is.musix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiXApplication.class, args);
+	}
+
+}
